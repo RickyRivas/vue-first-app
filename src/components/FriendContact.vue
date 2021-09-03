d48c9a7ef6258b28d6be27cfb18d09f695da5c61
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-09-03 15:38:17 -0500

    .

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click='toggleDetails'>{{ detailsAreVisible ? 'Hide Details' : 'Show Details'}}</button>
+    <ul v-if='detailsAreVisible'>
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "raul",
+        name: "Raul Saucedo",
+        phone: "9183128822",
+        email: "123@gmail.com",
+      },
+    };
+  },
+  methods: {
+      toggleDetails(){
+          this.detailsAreVisible = !this.detailsAreVisible;
+      }
+  },   
+};
+</script>  
--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import { createApp } from 'vue'
+// Import from a package
+import { createApp } from 'vue';
 
-createApp({}).mount('#app')
+//Import from custom package and components
+import App from './App.vue';
+
+import FriendContact from './components/FriendContact.vue' 
+
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
+   
