adfcfb395328bf4ecd5857d07ac9f37140ace565
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-09-03 20:36:42 -0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,82 @@
 <template>
   <section>
     <header>
-    <h1>My friends</h1>
+      <h1>My friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        v-for="friend of friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
     </ul>
   </section>
 </template>
 
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
         {
-          id: "raul",
-          name: "Raul Saucedo",
+          id: "person1",
+          name: "person one",
           phone: "9183128822",
           email: "123@gmail.com",
+          isFavorite: false,
         },
         {
-          id: "ariel",
-          name: "Ariel Salazar",
+          id: "person2",
+          name: "person two",
           phone: "9183576822",
           email: "456@gmail.com",
+          isFavorite: false,
         },
       ],
     };
+  },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email){
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      }
+      this.friends.push(newFriendContact)
+    },
+    deleteContact(friendId){
+      this.friends = this.friends.filter((friend) => friend.id !== friendId);
+    }
   },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: "Jost", sans-serif;
 }
 
 body {
@@ -67,7 +101,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -100,5 +135,17 @@ header {
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
 }
-
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
 </style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,30 +1,59 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(favorite)' : ''}}</h2>
     <button @click='toggleDetails'>{{ detailsAreVisible ? 'Hide Details' : 'Show Details'}}</button>
+    <button @click='toggleFavorite'>Toggle Favorite</button>
     <ul v-if='detailsAreVisible'>
-      <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>Email:</strong> {{ friend.email }}</li>
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
     </ul>
+    <button @click="$emit('delete', id)">Delete Contact</button>
   </li>
 </template>
 
 <script>
 export default {
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress',
+  //   'isFavorite'
+  // ],
+  emits: ['toggle-favorite', 'delete'],
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true,
+
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+    }
+  },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "raul",
-        name: "Raul Saucedo",
-        phone: "9183128822",
-        email: "123@gmail.com",
-      },
     };
   },
   methods: {
       toggleDetails(){
           this.detailsAreVisible = !this.detailsAreVisible;
+      },
+      toggleFavorite(){
+        this.$emit('toggle-favorite', this.id);
       }
   },   
 };
